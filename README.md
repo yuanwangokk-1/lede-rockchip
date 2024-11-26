23343e73a873d839e629867c5199bc57c7f4d049
unknown <yuanwangokk@proton.me>
2024-11-26 16:39:41 +0800

    main

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
-# lede-rockchip
+```
+https://github.com/DHDAXCW/lede-rockchip
+```
+
+## The source code is suitable for Rockchip OpenWrt SDK
+### Only supported devices:
+```
+embedfire_doornet1
+embedfire_doornet2
+embedfire_lubancat1
+embedfire_lubancat1n
+embedfire_lubancat2
+embedfire_lubancat2n
+friendlyarm_nanopi-r2c
+friendlyarm_nanopi-r2s
+friendlyarm_nanopi-r4s
+friendlyarm_nanopi-r4se
+friendlyarm_nanopi-r5s
+friendlyarm_nanopi-r5c
+friendlyarm_nanopi-r6c
+friendlyarm_nanopi-r6s
+friendlyarm_nanopc-t6
+hinlink_opc-h66k
+hinlink_opc-h68k
+hinlink_opc-h69k
+```
+
+### Next plan to add equipment:
+```
+hinlink_h88k
+```
+How to compile the OpenWrt firmware you need
+-
+Notice:
+-
+1. **NOT** compile with **root** user! ! !
+2. Domestic users had better prepare a ladder before compiling
+3. Default login IP 192.168.1.1 password password
+
+2. First install the Linux system, Debian 11 or Ubuntu LTS is recommended
+3. Install compilation dependencies
+
+   ```bash
+   sudo apt update -y
+   sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
+   bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib \
+   git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev libgmp3-dev \
+   libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-dev libreadline-dev \
+   libssl-dev libtool lrzsz mkisofs msmtp ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 \
+   python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools subversion swig texinfo \
+   uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
+   ```
+
+3. Download source code, update feeds and choose configuration
+
+   ```bash
+   git clone https://github.com/DHDAXCW/lede-rockchip
+   cd lede-rockchip
+   ./scripts/feeds update -a
+   ./scripts/feeds install -a
+   make menuconfig
+   ```
+
+5. `make -j8 download V=s` download dl library
+
+6. Enter `make -j10 V=s` (-j1 is followed by the number of threads. It is recommended to use single thread for the first compilation) to start compiling the firmware you want.
+
+This set of code is guaranteed to compile successfully. It includes all source codes of R24, including IPK.
+
+Second compilation:
+```bash
+cd lede
+git pull
+./scripts/feeds update -a && ./scripts/feeds install -a
+make defconfig
+make -j8 download
+make -j$(($(nproc) + 1)) V=s
+```
+
+If reconfiguration is required:
+```bash
+rm -rf ./tmp && rm -rf .config
+make menuconfig
+make -j$(($(nproc) + 1)) V=s
+```
+
+- Output path after compilation is complete：bin/targets
+
+- from upstream source code https://github.com/coolsnowwolf/lede
---
+++ target/linux/rockchip/image/armv8.mk
@@ -0,0 +1,215 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2020 Tobias Maedel
+
+# FIT will be loaded at 0x02080000. Leave 16M for that, align it to 2M and load the kernel after it.
+KERNEL_LOADADDR := 0x03200000
+
+define Device/embedfire_doornet1
+  DEVICE_VENDOR := EmbedFire
+  DEVICE_MODEL := DoorNet1
+  SOC := rk3328
+  BOOT_FLOW := pine64-bin
+  DEVICE_PACKAGES := kmod-usb-net-rtl8152 kmod-rtl8821cu
+endef
+TARGET_DEVICES += embedfire_doornet1
+
+define Device/embedfire_doornet2
+  DEVICE_VENDOR := EmbedFire
+  DEVICE_MODEL := DoorNet2
+  SOC := rk3399
+  BOOT_FLOW := pine64-bin
+  DEVICE_PACKAGES := kmod-r8168 -urngd
+endef
+TARGET_DEVICES += embedfire_doornet2
+
+define Device/embedfire_lubancat-1
+  DEVICE_VENDOR := EmbedFire
+  DEVICE_MODEL := LubanCat-1
+  SOC := rk3566
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-mt7921e wpad-openssl luci-app-modem
+endef
+TARGET_DEVICES += embedfire_lubancat-1
+
+define Device/embedfire_lubancat-1n
+  DEVICE_VENDOR := EmbedFire
+  DEVICE_MODEL := LubanCat-1N
+  SOC := rk3566
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-r8169 -urngd kmod-ata-ahci
+endef
+TARGET_DEVICES += embedfire_lubancat-1n
+
+define Device/radxa_zero-3e
+  DEVICE_VENDOR := RADXA
+  DEVICE_MODEL := ZERO-3E
+  SOC := rk3566
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-r8169 -urngd kmod-ata-ahci
+endef
+TARGET_DEVICES += radxa_zero-3e
+
+define Device/embedfire_lubancat-2
+  DEVICE_VENDOR := EmbedFire
+  DEVICE_MODEL := LubanCat-2
+  SOC := rk3568
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-ata-ahci kmod-ata-ahci-platform kmod-ata-core kmod-ata-ahci kmod-ata-ahci-platform kmod-ata-core
+endef
+TARGET_DEVICES += embedfire_lubancat-2
+
+define Device/embedfire_lubancat-2n
+  DEVICE_VENDOR := EmbedFire
+  DEVICE_MODEL := LubanCat-2N
+  SOC := rk3568
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-r8125 kmod-ata-ahci kmod-ata-ahci-platform kmod-ata-core
+endef
+TARGET_DEVICES += embedfire_lubancat-2n
+
+define Device/embedfire_lubancat-4
+  DEVICE_VENDOR := EmbedFire
+  DEVICE_MODEL := LubanCat-4
+  SOC := rk3588s
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-usb-serial-option uqmi kmod-mt7921e wpad-openssl
+endef
+TARGET_DEVICES += embedfire_lubancat-4
+
+define Device/embedfire_lubancat-5
+  DEVICE_VENDOR := EmbedFire
+  DEVICE_MODEL := LubanCat-5
+  SOC := rk3588
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-hwmon-pwmfan kmod-mt7921e kmod-r8125 wpad-openssl
+endef
+TARGET_DEVICES += embedfire_lubancat-5
+
+define Device/hinlink_common
+  DEVICE_VENDOR := HINLINK
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-hwmon-pwmfan kmod-mt7921e kmod-r8125 wpad-openssl luci-app-modem
+endef
+
+define Device/hinlink_opc-h66k
+$(call Device/hinlink_common)
+  DEVICE_MODEL := OPC-H66K
+  SOC := rk3568
+endef
+TARGET_DEVICES += hinlink_opc-h66k
+
+define Device/hinlink_opc-h68k
+$(call Device/hinlink_common)
+  DEVICE_MODEL := OPC-H68K
+  SOC := rk3568
+endef
+TARGET_DEVICES += hinlink_opc-h68k
+
+define Device/hinlink_opc-h69k
+$(call Device/hinlink_common)
+  DEVICE_MODEL := OPC-H69K
+  SOC := rk3568
+  DEVICE_PACKAGES += kmod-usb-serial-option uqmi
+endef
+TARGET_DEVICES += hinlink_opc-h69k
+
+define Device/hinlink_h88k
+  DEVICE_VENDOR := HINLINK
+  DEVICE_MODEL := H88K
+  SOC := rk3588
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-hwmon-pwmfan kmod-mt7921e kmod-r8125 wpad-openssl
+endef
+TARGET_DEVICES += hinlink_h88k
+
+define Device/friendlyarm_nanopi-r2c
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R2C
+  SOC := rk3328
+  BOOT_FLOW := pine64-bin
+  DEVICE_PACKAGES := kmod-usb-net-rtl8152
+endef
+TARGET_DEVICES += friendlyarm_nanopi-r2c
+
+define Device/friendlyarm_nanopi-r2s
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R2S
+  SOC := rk3328
+  BOOT_FLOW := pine64-bin
+  DEVICE_PACKAGES := kmod-usb-net-rtl8152
+endef
+TARGET_DEVICES += friendlyarm_nanopi-r2s
+
+define Device/friendlyarm_nanopi-r3s
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R3S
+  SOC := rk3566
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-r8168 -urngd
+endef
+TARGET_DEVICES += friendlyarm_nanopi-r3s
+
+define Device/friendlyarm_nanopi-r4s
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R4S
+  SOC := rk3399
+  BOOT_FLOW := pine64-bin
+  DEVICE_PACKAGES := kmod-r8168  kmod-rtl8821cu -urngd
+endef
+TARGET_DEVICES += friendlyarm_nanopi-r4s 
+
+define Device/friendlyarm_nanopi-r4se
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R4SE
+  SOC := rk3399
+  BOOT_FLOW := pine64-bin
+  DEVICE_PACKAGES := kmod-r8168 -urngd
+endef
+TARGET_DEVICES += friendlyarm_nanopi-r4se
+
+define Device/friendlyarm_nanopi-r5s
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R5S
+  SOC := rk3568
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-hwmon-pwmfan kmod-mt7921e kmod-r8125 wpad-openssl
+endef
+TARGET_DEVICES += friendlyarm_nanopi-r5s
+
+define Device/friendlyarm_nanopi-r5c
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R5C
+  SOC := rk3568
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-r8125
+endef
+TARGET_DEVICES += friendlyarm_nanopi-r5c
+
+define Device/friendlyarm_nanopc-t6
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPC T6
+  SOC := rk3588
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-hwmon-pwmfan kmod-mt7921e kmod-r8125 wpad-openssl
+endef
+TARGET_DEVICES += friendlyarm_nanopc-t6
+
+define Device/friendlyarm_nanopi-r6s
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R6S
+  SOC := rk3588s
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-hwmon-pwmfan kmod-mt7921e kmod-r8125 wpad-openssl
+endef
+TARGET_DEVICES += friendlyarm_nanopi-r6s
+
+define Device/friendlyarm_nanopi-r6c
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R6C
+  SOC := rk3588s
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-hwmon-pwmfan kmod-mt7921e kmod-r8125 wpad-openssl
+endef
+TARGET_DEVICES += friendlyarm_nanopi-r6c
+
